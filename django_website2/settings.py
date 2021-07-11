ef0e1af03e43bbfda63fd773f87e33c36a8efe23
pajannat <ytchisuga@gmail.com>
2021-07-11 22:39:40 +0900

    Herokuデプロイに向け設定追記

--- django_website2/settings.py
+++ django_website2/settings.py
@@ -130,3 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
