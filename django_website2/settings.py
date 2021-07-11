8f5c66e14c13c90020e6314b7be43c16599f4e8c
pajannat <ytchisuga@gmail.com>
2021-07-12 00:00:20 +0900

    Herokuデプロイに向け設定追記

--- django_website2/settings.py
+++ django_website2/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,6 +135,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
