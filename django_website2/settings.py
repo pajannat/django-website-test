ef46a4708d53dd502bc70bf325b4c4d5adecd99b
pajannat <ytchisuga@gmail.com>
2021-07-12 21:40:39 +0900

    Add

--- django_website2/settings.py
+++ django_website2/settings.py
@@ -132,6 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
